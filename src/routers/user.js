d3a885c03b64606a8cfe21a0dc29ca10a2f1b4bd
Vazeema-Siddiqui <svazeema@gmail.com>
2024-04-21 01:19:12 +0500

    Added login/logout functionality

--- src/routers/user.js
+++ src/routers/user.js
@@ -27,16 +27,30 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", auth, async (req, res) => {
+router.post("/users/logout", auth, async (req, res) => {
   try {
-    const users = await User.find({});
-    if (!users) {
-      return res.status(404).send();
-    }
-    res.send(users);
+    req.user.tokens = req.user.tokens.filter(
+      (token) => token.token !== req.token
+    );
+    await req.user.save();
+    res.send();
   } catch (e) {
     res.status(500).send();
   }
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {
