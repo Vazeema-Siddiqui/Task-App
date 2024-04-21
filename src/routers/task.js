147997e860b240d59adf5d377ee8282b6763f60b
Vazeema-Siddiqui <vazeema.siddiqui@techlogix.com>
2024-04-22 01:10:21 +0500

    Added pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -14,9 +14,20 @@ router.post("/tasks", auth, async (req, res) => {
 });
 
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
   try {
     // const tasks = await Task.find({owner:req.user._id});
-    await req.user.populate("tasks");
+    await req.user.populate({
+      path: "tasks",
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+      },
+    });
 
     res.send(req.user.tasks);
   } catch (e) {
